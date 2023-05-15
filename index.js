f78ba2a2fc50b54d844d35b23551d98859a4c6aa
Matheus <ymatheus.h@gmail.com>
2023-05-15 16:16:46 -0300

    first commit fork project

--- index.js
+++ index.js
@@ -1,72 +1,91 @@
-const { Client, LocalAuth, Chat } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const moment = require('moment-timezone');
-const colors = require('colors');
 const fs = require('fs');
 
 const client = new Client({
-    restartOnAuthFail: true,
-    puppeteer: {
-        headless: true,
-        args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
-    },
-    ffmpeg: './ffmpeg.exe',
-    authStrategy: new LocalAuth({ clientId: "client" })
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+  ffmpeg: './ffmpeg.exe',
+  authStrategy: new LocalAuth({ clientId: "client" })
 });
 const config = require('./config/config.json');
 
 client.on('qr', (qr) => {
-    console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan the QR below : `);
-    qrcode.generate(qr, { small: true });
+  console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan the QR below : `);
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.clear();
-    const consoleText = './config/console.txt';
-    fs.readFile(consoleText, 'utf-8', (err, data) => {
-        if (err) {
-            console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Console Text not found!`.yellow);
-            console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Already!`.green);
-        } else {
-            console.log(data.green);
-            console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Already!`.green);
-        }
-    });
+  console.clear();
+  const consoleText = './config/console.txt';
+  fs.readFile(consoleText, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Console Text not found!`.yellow);
+      console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Already!`.green);
+    } else {
+      console.log(data.green);
+      console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Already!`.green);
+    }
+  });
 });
 
 client.on('message', async (message) => {
-    const isGroups = message.from.endsWith('@g.us') ? true : false;
-    if ((isGroups && config.groups) || !isGroups) {
-        if (message.type == "image" || message.type == "video" || message.type  == "gif") {
-            client.sendMessage(message.from, "*[⏳]* Loading..");
-            try {
-                const media = await message.downloadMedia();
-                client.sendMessage(message.from, media, {
-                    sendMediaAsSticker: true,
-                    stickerName: config.name, // Sticker Name = Edit in 'config/config.json'
-                    stickerAuthor: config.author // Sticker Author = Edit in 'config/config.json'
-                }).then(() => {
-                    client.sendMessage(message.from, "*[✅]* Successfully!");
-                });
-            } catch {
-                client.sendMessage(message.from, "*[❎]* Failed!");
-            }
-        } else if (message.type == "sticker") {
-            client.sendMessage(message.from, "*[⏳]* Loading..");
-            try {
-                const media = await message.downloadMedia();
-                client.sendMessage(message.from, media).then(() => {
-                    client.sendMessage(message.from, "*[✅]* Successfully!");
-                });  
-            } catch {
-                client.sendMessage(message.from, "*[❎]* Failed!");
-            }
-        } else {
-            client.getChatById(message.id.remote).then(async (chat) => {
-                await chat.sendSeen();
-            });
-        }
-    }
+  if (message.broadcast) return;
+
+  if (!config.groups) {
+    const chat = await message.getChat();
+    if (chat.isGroup) return;
+  }
+
+  // console.log(message);
+
+  if (!message.hasMedia) return;
+  if (!message.body.toLowerCase().includes('/bot')) return;
+
+  const stickerTypes = ['image', 'video', 'gif'];
+  if (stickerTypes.includes(message.type)) {
+    await uploadSticker(client, message);
+  }
+
+  if (message.type == "sticker") {
+    await uploadMedia(client, message);
+  }
+
+  // client.getChatById(message.id.remote).then(async (chat) => {
+  //   await chat.sendSeen();
+  // });
 });
+
+async function uploadSticker(client, message) {
+  client.sendMessage(message.from, "*[⏳]* Loading..");
+  try {
+    const media = await message.downloadMedia();
+    client.sendMessage(message.from, media, {
+      sendMediaAsSticker: true,
+      stickerName: config.name, // Sticker Name = Edit in 'config/config.json'
+      stickerAuthor: config.author // Sticker Author = Edit in 'config/config.json'
+    }).then(() => {
+      client.sendMessage(message.from, "*[✅]* Successfully!");
+    });
+  } catch {
+    client.sendMessage(message.from, "*[❎]* Failed!");
+  }
+}
+
+async function uploadMedia(client, message) {
+  client.sendMessage(message.from, "*[⏳]* Loading..");
+  try {
+    const media = await message.downloadMedia();
+    client.sendMessage(message.from, media).then(() => {
+      client.sendMessage(message.from, "*[✅]* Successfully!");
+    });
+  } catch {
+    client.sendMessage(message.from, "*[❎]* Failed!");
+  }
+}
 
 client.initialize();
